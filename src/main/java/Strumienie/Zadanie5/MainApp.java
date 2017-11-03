47b7ad93a54ac98fdf12c7ad87d704f82622662f
MSI\Mateusz <mateuszdeptula1@gmail.com>
2017-11-03 11:35:59 +0100

    Dodane zadania ze strumieni. Trzecie zadanie z kolecji nadal niepoprawione.

--- src/main/java/Strumienie/Zadanie5/MainApp.java
+++ src/main/java/Strumienie/Zadanie5/MainApp.java
@@ -2,9 +2,6 @@ package Strumienie.Zadanie5;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.OptionalDouble;
-import java.util.stream.Collectors;
-
 public class MainApp {
 
     public static void main(String[] args) {
